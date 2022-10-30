01bee810e33bf866190c0b59aaf9b3ade9560daa
Dhruv <Dhruvsinghal9876@gmail.com>
2022-10-31 00:09:04 +0530

    add orders table, place order'

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-
+import { initAdmin } from './admin'
 let addToCart=document.querySelectorAll('.add-to-cart')
 let cartCounter=document.querySelector('#cartCounter')
 function updateCart(pizza){
@@ -19,3 +19,13 @@ addToCart.forEach((btn)=>{
         updateCart(pizza)
     })
 }) 
+
+//Remove alert message after X seconds
+const alertMsg=document.querySelector('#success-alert')
+if(alertMsg){
+    setTimeout(() => {
+        alertMsg.remove()
+    }, 2000);
+}
+
+initAdmin()
--- routes/web.js
+++ routes/web.js
@@ -1,8 +1,14 @@
 const homeController=require('../app/http/controllers/homeController')
 const authController=require('../app/http/controllers/authController')
 const cartController=require('../app/http/controllers/customers/cartController')
+const orderController=require('../app/http/controllers/customers/orderController')
 const { authorize } = require('passport')
+const AdminOrderController=require('../app/http/controllers/admin/orderController')
+
+//Middlewares
 const guest = require('../app/http/middlewares/guest')
+const auth=require('../app/http/middlewares/auth')
+const admin=require('../app/http/middlewares/admin')
 
 function initRoutes(app){
     app.get('/',homeController().index)
@@ -15,6 +21,14 @@ function initRoutes(app){
 
     app.get('/cart',cartController().index);
     app.post('/update-cart',cartController().update);
+
+    //Customer Routes
+    app.post('/orders', auth, orderController().store)
+    app.get('/customer/orders',auth,orderController().index)
+
+    app.get('/admin/orders',admin, AdminOrderController().index)
+    // app.post('/admin/order/status', statusController().update)
+
 }
 
 module.exports=initRoutes 
